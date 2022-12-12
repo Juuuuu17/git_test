7e989b85a72d1afb54e76aa08a7cf2897ae51de9
bochi <boschluckas@gmail.com>
2022-12-12 09:06:50 +0100

    update

--- fichier.py
+++ fichier.py
@@ -1,2 +1,11 @@
-with open("text.txt", "a") as f:
-    f.write("Hello World\n")
+import re
+
+# Open the file and read its contents
+with open('text.txt', 'r') as f:
+    text = f.read()
+
+# Use the findall() method to find all occurrences of the regex
+matches = re.findall(r'luck[a-z]*', text)
+
+# Print the matches
+print(matches)
